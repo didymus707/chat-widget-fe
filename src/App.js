e787755213e318b921fea13b9afe66c7290abb7b
Adewale Orotayo <didymus7007@gmail.com>
2021-04-18 12:57:37 +0100

    Installed react-router-dom and react-toastify added components, dashboard, register nad login Made changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+// import axios from 'axios';
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Redirect
+} from 'react-router-dom';
 import './App.css';
+import Register from './components/Register'
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
-  const [all, setAll] = useState(null);
+  // const [users, setUsers] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000',
-  });
+  // const axiosInstance = axios.create({
+  //   baseURL: 'http://localhost:5000',
+  // });
 
-  const getData = async () => {
-    try {
-      const request = await axiosInstance.get('/all');
-      const response = await request.data;
-      setAll(response);
-  
-    } catch (error) {
-      console.log(error);
-    }
+  const setAuth = boolean => {
+    setIsAuthenticated(boolean);
   }
 
-  useEffect(() => {
-    getData();
-  })
+  // const getData = async () => {
+  //   try {
+  //     const req = await axiosInstance.get('/api/get/users');
+  //     const res = await req.data;
+  //     setUsers(res)
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getData();
+  // })
 
   return (
-    <div>
-      { all ? <div>{ all }</div> : null }
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route 
+              exact
+              path='/register' 
+              render={props => !isAuthenticated ? <Register {...props} setAuth={setAuth} /> : <Redirect to='/login' />} 
+            />
+            <Route 
+              exact 
+              path='/login' 
+              render={props => !isAuthenticated ? <Login {...props} setAuth={setAuth} /> : <Redirect to='/dashboard' />}
+            />
+            <Route 
+              exact
+              path='/dashboard' 
+              render={props => isAuthenticated ? <Dashboard {...props} setAuth={setAuth} /> : <Redirect to='/login' />} 
+            />
+          </Switch>
+        </div>
+      </Router>
+
+      {/* <div className="users">
+        <h1>Users</h1>
+        {users}
+        {console.log(users)}
+      </div> */}
+    </>
   )
 }
 
